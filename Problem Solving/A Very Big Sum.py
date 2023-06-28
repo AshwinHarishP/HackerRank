dc2d345e7dd0ca7a1ea5aa1d2ca138e99a61ad0e
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-06-28 21:46:08 +0530

    Add files via upload

---
+++ Problem Solving/A Very Big Sum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def aVeryBigSum(ar):
+    Sum = 0
+    for i in range(len(ar)):
+        Sum+=ar[i]
+    return Sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    
