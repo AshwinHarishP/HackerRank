ff16adb10b74169f75d396c3e4a96b69e482cf6d
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-08-24 23:35:51 +0530

    Add files via upload

---
+++ Problem Solving/sherlock and squares.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    root_a = int(a**0.5)
+    root_b = int(b**0.5)
+    
+    count = root_b - root_a
+    if root_a*root_a == a:
+        count+=1
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
