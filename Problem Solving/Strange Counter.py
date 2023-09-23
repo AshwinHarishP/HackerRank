d985e0b145b38cafad4e61884e11d058a595ad57
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-23 13:03:20 +0530

    Create Strange Counter.py

---
+++ Problem Solving/Strange Counter.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    n = 4
+    while (t >= n):
+        n = (2*n) + 2
+    return n - t
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+S
