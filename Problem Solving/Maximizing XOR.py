6751af30fe9e5aab70c66c3865f30a7049951920
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-26 16:04:25 +0530

    Create Maximizing XOR.py

---
+++ Problem Solving/Maximizing XOR.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximizingXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER l
+#  2. INTEGER r
+#
+
+def maximizingXor(l, r):
+    xor_list = []
+    for i in range(l, r+1):
+        for j in range(i,r+1):
+            xor_list.append(i ^ j)
+    return max(xor_list)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input().strip())
+
+    r = int(input().strip())
+
+    result = maximizingXor(l, r)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
