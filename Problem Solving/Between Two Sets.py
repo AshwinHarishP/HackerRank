48b075cf220705c4f0f4496a70e898021c9b17d7
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-10-01 20:47:52 +0530

    Create Between Two Sets.py

---
+++ Problem Solving/Between Two Sets.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    a_lcm = math.lcm(*a)
+    b_gcd = math.gcd(*b)
+    count = 0
+    
+    for i in range(a_lcm, b_gcd+1):
+        if i % a_lcm == 0 and b_gcd % i == 0:
+            count += 1
+    return count
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
